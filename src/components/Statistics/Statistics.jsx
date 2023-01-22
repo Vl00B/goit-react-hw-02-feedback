e089ef710fee73b80b18dfa0e89b8a27e49ff389
vl00b <110897682+Vl00B@users.noreply.github.com>
2023-01-22 22:35:34 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -10,6 +10,6 @@ export const Statistics = ({
     <li>neutral:{neutral}</li>
     <li>bad:{bad}</li>
     <li>Total:{total}</li>
-    <li>{good === 0 ? '0' : positivePercentage} %</li>
+    <li>Positive feedback {good === 0 ? '0' : positivePercentage} %</li>
   </ul>
 );
