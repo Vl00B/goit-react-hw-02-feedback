d95729604dea726ab5828ef1a0bdc3ef424cc060
vl00b <110897682+Vl00B@users.noreply.github.com>
2023-02-27 21:09:56 +0200

    _

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,14 +1,17 @@
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import s from './Section.module.css';
 
-export const Section = ({ title, children }) => {
-  return (
-    <section className={s.container}>
-      <h2 className={s.title}>{title}</h2>
-      {children}
-    </section>
-  );
-};
+export class Section extends Component {
+  render() {
+    return (
+      <section className={s.container}>
+        <h2 className={s.title}>{this.props.title}</h2>
+        {this.props.children}
+      </section>
+    );
+  }
+}
 
 Section.propTypes = {
   title: PropTypes.string.isRequired,
