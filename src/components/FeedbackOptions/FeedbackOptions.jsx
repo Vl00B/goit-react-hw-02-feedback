bf6e641d1db935257cf2ffa7e859403e4f2226db
vl00b <110897682+Vl00B@users.noreply.github.com>
2023-02-27 21:08:52 +0200

    _

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,24 +1,27 @@
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import s from './FeedbackOptions.module.css';
 
-export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-  return (
-    <ul className={s.feedback_list}>
-      {options.map(option => (
-        <li key={option} className={s.option}>
-          <button
-            className={s.btn}
-            type="button"
-            value={option}
-            onClick={() => onLeaveFeedback(option)}
-          >
-            {option}
-          </button>
-        </li>
-      ))}
-    </ul>
-  );
-};
+export class FeedbackOptions extends Component {
+  render() {
+    const { options, onLeaveFeedback } = this.props;
+    return (
+      <ul className={s.feedback_list}>
+        {options.map(option => (
+          <li key={option} className={s.option}>
+            <button
+              type="button"
+              value={option}
+              onClick={() => onLeaveFeedback(option)}
+            >
+              {option}
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
 
 FeedbackOptions.propTypes = {
   onLeaveFeedback: PropTypes.func.isRequired,
