671bad36f82b1901bd921382f24646d45c91a89e
vl00b <110897682+Vl00B@users.noreply.github.com>
2023-02-27 21:08:04 +0200

    _

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,26 +1,24 @@
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import s from './Statistics.module.css';
 
-export const Statistics = ({
-  good,
-  neutral,
-  bad,
-  total,
-  positivePercentage,
-}) => {
-  return (
-    <>
-      <h3>Feedback</h3>
-      <ul className={s.options}>
-        <li>Positive: {good}</li>
-        <li>Neutral: {neutral}</li>
-        <li>Negative: {bad}</li>
-        <li>Total: {total}</li>
-        <li>Positive percentage {good === 0 ? '0' : positivePercentage}%</li>
-      </ul>
-    </>
-  );
-};
+export class Statistics extends Component {
+  render() {
+    const { good, neutral, bad, total, positivePercentage } = this.props;
+    return (
+      <>
+        <h3>Feedback</h3>
+        <ul className={s.options}>
+          <li>Positive: {good}</li>
+          <li>Neutral: {neutral}</li>
+          <li>Negative: {bad}</li>
+          <li>Total: {total}</li>
+          <li>Positive percentage {good === 0 ? '0' : positivePercentage}%</li>
+        </ul>
+      </>
+    );
+  }
+}
 
 Statistics.propTypes = {
   good: PropTypes.number.isRequired,
